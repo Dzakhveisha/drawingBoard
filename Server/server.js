ae58558f92982d7dd21d7b3415c492d0bcf5c113
Dzakhveisha <Dzakhveisha@gmail.com>
2021-05-25 13:15:46 +0300

    start audio

--- Server/server.js
+++ Server/server.js
@@ -6,11 +6,24 @@ const port = 6969;
 const server = http.createServer(express);
 const wss = new WebSocket.Server({ server })
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(data) {
+let idCounter = 0;
+let members = [];
 
+wss.on('connection', function connection(wsClient) {
+    idCounter++;
+    let isFirst = false;
+    if (members.length === 0){
+        isFirst = true;
+    }
+    wsClient.send(JSON.stringify({type: 'newId', id: idCounter, isFirst: isFirst}));
+    members.push(idCounter);
+    wsClient.on('message', function incoming(data) {
+        let dataAr = JSON.parse(data);
+        if (dataAr["type"] === "exit"){
+            members.splice(members.indexOf(dataAr["id"]), 1);
+        }
         wss.clients.forEach(function each(client) {
-             if (client !== ws && client.readyState === WebSocket.OPEN) {
+             if (client !== wsClient && client.readyState === WebSocket.OPEN) {
                 client.send(data);
              }
         })
@@ -20,3 +33,5 @@ wss.on('connection', function connection(ws) {
 server.listen(port, function() {
     console.log(`Server is listening on ${port}!`)
 })
+
+
